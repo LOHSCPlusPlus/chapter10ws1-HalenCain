be7d9ab607501fb1ba1340d8ec5b4083b21cb426
HALENCAIN <>
2022-03-11 16:50:43 +0000

    WS1

--- Point.h
+++ Point.h
@@ -1,6 +1,13 @@
 class Point{
     public:
+        Point (double xVal, double yVal);
         Point();
+        double getX();
+        double getY();
+        void setX(double xVal);
+        void setY(double yVal);
+        void print();
+        double calcDistance(Point other);
     private:
         double x;
         double y;
