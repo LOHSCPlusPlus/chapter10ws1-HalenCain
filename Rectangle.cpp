a6648ac5e442439844f4b936dcfc190ed901ab0b
HALENCAIN <>
2022-04-01 05:21:26 +0000

    WS 3 complete

--- Rectangle.cpp
+++ Rectangle.cpp
@@ -1,1 +1,15 @@
 #include "Rectangle.h"
+#include <iostream>
+using namespace std;
+
+Rectangle::Rectangle(Line l, Line h) {
+  length = l;
+  height = h;
+}
+void Rectangle::print(){
+  cout << "Lenght: " << length.lineLength() << endl;
+  cout << "Height: " << height.lineLength() << endl;
+}
+double Rectangle::calcArea(){
+  return length.lineLength() * height.lineLength();
+}
--- RectangleClient.cpp
+++ RectangleClient.cpp
@@ -1,5 +1,17 @@
 #include "Rectangle.h"
+#include <iostream>
+using namespace std;
 
 int main() {
+    Point start1(3.2, 7.8);
+    Point end1(4.5, 9.3);
+    Line h(start1, end1);
+    Point start2(2.2, 2.5);
+    Point end2(4.5, 10.2);
+    Line l(start2, end2);
+    Rectangle rectangle(l, h);
+    rectangle.print();
+    cout << "Area: " << rectangle.calcArea();
+    cout << endl;
     return 0;
 }
